d8aea1914c4d065bf2043b345bfd619ce6f9399c
James Halliday <mail@substack.net>
2014-11-30 22:04:08 -0800

    first part of protobuf rpc and the new multiplex

--- index.js
+++ index.js
@@ -1,56 +1,29 @@
 var multiplex = require('multiplex');
 var inherits = require('inherits');
 var Duplex = require('readable-stream').Duplex;
+var protobuf = require('protocol-buffers');
+var fs = require('fs');
 var through = require('through2');
-var split = require('split');
-var isarray = require('isarray');
-var has = require('has');
-var defined = require('defined');
+
+var messages = protobuf(fs.readFileSync(__dirname + '/schema.proto'));
+var TYPE = messages.TYPE;
+var RPC = messages.RPC;
 
 module.exports = Rep;
 inherits(Rep, Duplex);
 
-var codes = {
-    handshake: 0, available: 1, request: 2,
-    hashes: 3, since: 4, close: 5
-};
-
-function Rep (opts, fn) {
+function Rep (fn) {
     var self = this;
-    if (!(this instanceof Rep)) return new Rep(opts, fn);
+    if (!(this instanceof Rep)) return new Rep(fn);
     Duplex.call(this);
-    if (typeof opts === 'function') {
-        fn = opts;
-        opts = {};
-    }
-    if (!opts) opts = {};
     
-    this._mplex = multiplex(function (stream, id) {
-        if (has(self._hashes, id)) {
-            var r = self._hashes[id];
-            if (r) self.emit('response', r.hash, stream, r.meta);
-        }
-    });
-    this._mplex.on('readable', function () {
-        if (self._reading) {
-            self._reading = false;
-            self._read();
+    this._mplex = multiplex({}, function (stream, ch) {
+        if (ch === '0') {
+            stream.pipe(self._handleRPC());
         }
     });
     
-    this._rpc = this._mplex.createStream(0);
-    var sp = split(JSON.parse);
-    sp.on('error', function () {});
-    this._rpc.pipe(sp).pipe(this._handleRPC());
-    
-    this._id = defined(
-        opts.id,
-        Math.floor(Math.random() * Math.pow(16,8)).toString(16)
-    );
-    this._rpc.write(JSON.stringify([
-        codes.handshake, this._id, defined(opts.meta, {})
-    ]) + '\n');
-    
+    this._rpc = this._mplex.createStream('0');
     this._provided = {};
     this._requested = {};
     this._hashes = {};
@@ -74,91 +47,37 @@ Rep.prototype._write = function (buf, enc, next) {
     return this._mplex._write(buf, enc, next);
 };
 
+Rep.prototype.destroy = function () {
+    this._mplex.destroy();
+};
+
 Rep.prototype._handleRPC = function () {
     var self = this;
-    return through.obj(function (row, enc, next) {
-        if (!isarray(row)) return next();
-        if (row[0] === codes.handshake) {
-            if (self._id > row[1]) {
-                self._even = true;
-            }
-            else if (self._id < row[1]) {
-                self._even = false;
-            }
-            else {
-                return self.push(null);
-            }
-            self.emit('handshake', row[1], row[2]);
-        }
-        else if (row[0] === codes.request) {
-            var hashes = isarray(row[1]) ? row[1] : [ row[1] ];
-            var hs = {}, rs = {};
-            var pending = hashes.length;
-            
-            hashes.forEach(function (h) {
-                if (!has(self._provided, h)) {
-                    if (-- pending === 0) done();
-                    return false;
-                }
-                self._loader(h, function (err, r, meta) {
-                    if (r) {
-                        var ix = self._index ++ * 2 + (self._even ? 1 : 2);
-                        rs[h] = { stream: r, index: ix };
-                        hs[h] = { index: ix, meta: defined(meta, {}) };
-                    }
-                    if (-- pending === 0) done();
-                });
-            });
-            
-            function done () {
-                var cmd = [ codes.hashes, hs ];
-                self._rpc.write(JSON.stringify(cmd) + '\n');
-                
-                Object.keys(rs).forEach(function (hash) {
-                    var r = rs[hash].stream, index = rs[hash].index;
-                    var stream = self._mplex.createStream(index);
-                    r.pipe(stream);
-                });
-            }
-        }
-        else if (row[0] === codes.available) {
-            var hashes = isarray(row[1]) ? row[1] : [ row[1] ];
-            var hs = hashes.filter(function (h) {
-                return !has(self._provided, h);
-            });
-            self.emit('available', hs);
-        }
-        else if (row[0] === codes.hashes) {
-            Object.keys(row[1] || {}).forEach(function (hash) {
-                if (has(self._requested, hash)) {
-                    var r = row[1][hash];
-                    if (!r) return;
-                    var index = r.index;
-                    self._hashes[index] = { hash: hash, meta: r.meta };
-                }
-            });
-        }
-        else if (row[0] === codes.since) {
-            self.emit('since', row[1]);
-        }
-        else if (row[0] === codes.close) {
-            self._closed.remote = true;
-            if (self._closed.local) {
-                self.emit('close');
-                self.push(null);
-                return;
-            }
-        }
-        next();
+    return through.obj(function (buf, enc, next) {
+        var msg = decode(buf);
+        if (!msg) return self.destroy();
+        console.error('msg=', msg);
     });
+    
+    function decode (buf) {
+        try { return RPC.decode(buf) }
+        catch (err) { return null }
+    }
 };
 
 Rep.prototype.since = function (seq) {
+    /*
     var cmd = [ codes.since, seq ];
     this._rpc.write(JSON.stringify(cmd) + '\n');
+    */
 };
 
 Rep.prototype.provide = function (hashes) {
+    this._rpc.write(RPC.encode({
+        type: TYPE.AVAILABLE,
+        hashes: hashes
+    }));
+    /*
     var self = this;
     if (!isarray(hashes) && hashes && typeof hashes === 'object') {
         Object.keys(hashes).forEach(function (key) {
@@ -173,9 +92,13 @@ Rep.prototype.provide = function (hashes) {
     }
     var cmd = [ codes.available, hashes ];
     this._rpc.write(JSON.stringify(cmd) + '\n');
+    */
 };
 
 Rep.prototype.request = function (hashes) {
+    console.error(hashes);
+    
+    /*
     var self = this;
     if (!isarray(hashes)) hashes = [ hashes ];
     hashes.forEach(function (h) {
@@ -183,13 +106,16 @@ Rep.prototype.request = function (hashes) {
     });
     var cmd = [ codes.request, hashes ];
     this._rpc.write(JSON.stringify(cmd) + '\n');
+    */
 };
 
 Rep.prototype.close = function () {
+    /*
     this._rpc.write(JSON.stringify([ codes.close ]) + '\n');
     this._closed.local = true;
     if (this._closed.remote) {
         this.emit('close');
         this.push(null);
     }
+    */
 };
